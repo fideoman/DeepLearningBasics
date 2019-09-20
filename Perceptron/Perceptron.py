dc259741b5c4421addd23b84c9db0e027db8ebe3
Isaac Silva <idsl1012@gmail.com>
2019-09-20 09:48:57 -0400

    Isaac: Initial commit for SL Perceptron class.

---
+++ Perceptron/Perceptron.py
@@ -0,0 +1,71 @@
+'''
+Created on 20-09-2019
+
+@author: Isaac Silva Luna
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn.metrics.accuracy_score as accuracy_score
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+    def __init__ (self):
+        '''
+        Constructor
+        '''
+        self.w = None
+        self.b = None
+    
+    def model(self, x):
+        return 1 if (np.dot(self.w, x) >= self.b) else 0
+    
+    def predict(self, X):
+        Y = []
+        for x in X:
+            result = self.model(x)
+            Y.append(result)
+            return np.array(Y)
+    
+    def fit(self, X, Y, epochs = 1, lr = 1):
+    
+        self.w = np.ones(X.shape[1])
+        self.b = 0
+
+        accuracy = {}
+        max_accuracy = 0
+
+        wt_matrix = []
+
+        for i in range(epochs):
+            for x, y in zip(X, Y):
+                y_pred = self.model(x)
+                if y == 1 and y_pred == 0:
+                    self.w = self.w + lr * x
+                    self.b = self.b - lr * 1
+                elif y == 0 and y_pred == 1:
+                    self.w = self.w - lr * x
+                    self.b = self.b + lr * 1
+
+            wt_matrix.append(self.w)    
+
+            accuracy[i] = accuracy_score(self.predict(X), Y)
+            if (accuracy[i] > max_accuracy):
+                max_accuracy = accuracy[i]
+                j = i
+                chkptw = self.w
+                chkptb = self.b
+
+        self.w = chkptw
+        self.b = chkptb
+
+        print(max_accuracy,j)
+
+        plt.plot(accuracy.values())
+        plt.xlabel("Epoch #")
+        plt.ylabel("Accuracy")
+        plt.ylim([0, 1])
+        plt.show()
+
+        return np.array(wt_matrix)        
